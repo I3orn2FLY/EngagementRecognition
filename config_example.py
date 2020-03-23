ff972c90c432077a968fd8090927af92b4d86667
Kenessary Koishybay <kenessary.koishybay@nu.edu.kz>
2020-03-23 11:44:33 +0600

    config

--- config_example.py
+++ config_example.py
@@ -1,8 +1,8 @@
 import os
 
 # SPLIT_METHOD = "CHILD"
-# SPLIT_METHOD = "SESSION"
-SPLIT_METHOD = "RANDOM"
+SPLIT_METHOD = "SESSION"
+# SPLIT_METHOD = "RANDOM"
 
 # WITH FACE, BODY, HANDS (411) =>
 # FEAT_NUM = 411
@@ -13,24 +13,51 @@ SPLIT_METHOD = "RANDOM"
 # WITH FACE ONLY (210) =>
 FEAT_NUM = 70 * 3
 
-SEQ_LENGTH = 5
+SEQ_LENGTH = 1
 
 NB_CLASS = 6
-CSV_FILE = "/media/kenny/Extra/EngagementRecognition/Data/ASD_28_result.csv"
+CSV_FILE = "/media/kenny/Extra/EngagementRecognition/Data/28_with_filenames.csv"
+IMAGES_DIR = "/media/kenny/Extra/EngagementRecognition/Data/croped_images_28children"
+CSV_DIR = "/media/kenny/Extra/EngagementRecognition/Data"
 VARS_DIR = "/media/kenny/Extra/EngagementRecognition/VARS"
 
 MODEL_PATH = os.sep.join(["weights", "BiLSTM_" + SPLIT_METHOD + "_" + str(SEQ_LENGTH) + "_" + str(FEAT_NUM) + ".pt"])
 
-if SEQ_LENGTH == 1:
-    MODEL_PATH = MODEL_PATH.replace("BiLSTM", "NN")
+# not for change
+use_CNN = False
+
+DEVICE = "cuda:0"
+
+CNN_FEAT_EXTRACT_BATCHSIZE = 512
 
 if SEQ_LENGTH == 1:
-    BATCH_SIZE = 16384
+    # change value here
+    use_CNN = True
+    # use_CNN = False
+
+    if use_CNN:
+        MODEL_PATH = MODEL_PATH.replace("BiLSTM", "CNN").replace("_" + str(FEAT_NUM) + ".pt", ".pt")
+        FEAT_NUM = 1024
+    else:
+        MODEL_PATH = MODEL_PATH.replace("BiLSTM", "NN")
+else:
+    use_CNN = False
+
+if SEQ_LENGTH == 1:
+    if use_CNN:
+        BATCH_SIZE = 256
+    else:
+        BATCH_SIZE = 16384
+
 elif SEQ_LENGTH == 5:
     BATCH_SIZE = 8192
+
 elif SEQ_LENGTH == 25:
     BATCH_SIZE = 4096
+
 elif SEQ_LENGTH == 50:
     BATCH_SIZE = 2048
+
 else:
     BATCH_SIZE = 1024
+
